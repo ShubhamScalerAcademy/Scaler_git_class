b16dcb369fb8630c0b408f172eaf44bea38f1f1e
ShubhamScalerAcademy <168635809+ShubhamScalerAcademy@users.noreply.github.com>
2024-05-05 15:54:37 +0530

    Add files via upload

---
+++ Git/A.java
@@ -0,0 +1,12 @@
+public class A {
+
+    private int a;
+    private int b;
+    private int c;
+    private int d;
+
+
+
+    
+
+}
---
+++ Git/D.java
@@ -0,0 +1,15 @@
+public class D {
+
+
+    private int value;
+
+    void printValues(){
+        
+        System.out.println(value);
+    }
+
+
+
+
+    
+}
